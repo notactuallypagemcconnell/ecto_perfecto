5806872903a1e7382753783666a15850521a3504
Bobby 'Bobdawg' Grayson <bobbbygrayson@gmail.com>
2018-09-25 12:03:47 -0400

    flesh out interface to albums/artists

--- lib/interface/music.ex
+++ lib/interface/music.ex
@@ -4,31 +4,45 @@ defmodule EctoPerfecto.Interface.Music do
   alias EctoPerfecto.{Album, Artist, Repo}
 
   def search(:album, title) do
-    query =
-      from lawn_boy in Album,
-      where: ilike(lawn_boy.title, ^title),
-      select: lawn_boy
-
-    case Repo.all(query) do
-      []     -> {:error, "Not Found"}
-      result -> {:ok, result}
-    end
+    title
+    |> album_search_query
+    |> Repo.all
+    |> handle_result
   end
 
   def search(:artist, name) do
-    query =
-      from phish in Artist,
-      where: ilike(phish.name, ^name),
-      select: phish,
-      preload: [:albums]
-
-    case Repo.all(query) do
-      []     -> {:error, "Not Found"}
-      result -> {:ok, result}
-    end
+    name
+    |> artist_search_query
+    |> Repo.all
+    |> handle_result
   end
 
   def search(_, _) do
     {:error, "Search interface not defined for input"}
+  end
+
+  defp artist_search_query(name) do
+    from phish in Artist,
+    where: ilike(phish.name, ^name),
+    select: phish,
+    preload: [:albums]
+  end
+
+  defp album_search_query(title) do
+    from lawn_boy in Album,
+    where: ilike(lawn_boy.title, ^title),
+    select: lawn_boy
+  end
+
+  defp handle_result([]) do
+    {:error, "Not Found"}
+  end
+
+  defp handle_result(result) when is_list(result) do
+    {:ok, result}
+  end
+
+  defp handle_result(_) do
+    {:error, "something weird went down"}
   end
 end
