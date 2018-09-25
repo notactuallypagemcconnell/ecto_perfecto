eab1fd33ede4f80366df147c9548848bf0a82776
Bobby 'Bobdawg' Grayson <bobbbygrayson@gmail.com>
2018-09-25 11:55:45 -0400

    add another interface for albums

--- lib/interface/music.ex
+++ lib/interface/music.ex
@@ -1,16 +1,29 @@
 defmodule EctoPerfecto.Interface.Music do
   import Ecto.Query
 
-  alias EctoPerfecto.{Artist, Repo}
+  alias EctoPerfecto.{Album, Artist, Repo}
+
+  def search(:album, title) do
+    query =
+      from lawn_boy in Album,
+      where: ilike(lawn_boy.title, ^title),
+      select: lawn_boy
+
+    case Repo.all(query) do
+      []     -> {:error, "Not Found"}
+      result -> {:ok, result}
+    end
+  end
 
   def search(:artist, name) do
     query =
-      from david_bowie in Artist,
-      where: ilike(david_bowie.name, ^name),
-      select: david_bowie,
+      from phish in Artist,
+      where: ilike(phish.name, ^name),
+      select: phish,
       preload: [:albums]
+
     case Repo.all(query) do
-      [] -> {:error, "Not Found"}
+      []     -> {:error, "Not Found"}
       result -> {:ok, result}
     end
   end
